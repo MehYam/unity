e2b83570d93b258b86b2bbcb148111d97a2536b5
Kai Arnold <cdn_kai@yahoo.com>
2013-09-09 11:28:30 -0700

    Can now log into the playing1 SF extension

---
+++ playing1/Assets/scripts/SmartFox/SmartFoxBroker.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+// this class will broker messages between us and the server
+public sealed class SmartFoxBroker
+{
+    static public readonly string ExtName = "playing1";
+    static public readonly string ExtClass = "Playing1ServerExtension";
+}
---
+++ playing1server/src/Playing1ServerExtension.java
@@ -0,0 +1,13 @@
+import com.smartfoxserver.v2.extensions.SFSExtension;
+
+
+public final class Playing1ServerExtension extends SFSExtension
+{
+
+	@Override
+	public void init()
+	{
+		// TODO Auto-generated method stub
+	}
+
+}
