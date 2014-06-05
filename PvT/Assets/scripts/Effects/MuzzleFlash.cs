c6947fc31fc692cc5eafe86f6447792ff712c4a9
Kai Arnold <cdn_kai@yahoo.com>
2014-06-04 23:49:56 -0700

    created muzzle flash animations

---
+++ PvT/Assets/scripts/Effects/MuzzleFlash.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuzzleFlash : MonoBehaviour
+{
+    void OnFlashComplete()
+    {
+        GetComponent<Animator>().enabled = false;
+    }
+}
