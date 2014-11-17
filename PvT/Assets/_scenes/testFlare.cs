7b08521e977392b7388b98e07089c07b9e6cc8d5
Kai Arnold <cdn_kai@yahoo.com>
2014-11-16 18:24:33 -0800

    Flare animation for ship upgrades.

---
+++ PvT/Assets/_scenes/testFlare.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class testFlare : MonoBehaviour
+{
+    public GameObject ReplaceWith;
+
+	// Use this for initialization
+	void Start()
+    {
+        StartCoroutine(DoReplace());	
+	}
+	
+    IEnumerator DoReplace()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        // allocate the new ship
+        var newShip = (GameObject)Instantiate(ReplaceWith);
+        newShip.transform.position = transform.position;
+
+        // KAI: here you'd transfer the physics properties over and set IGame.player
+
+        // move the animation over to the new thing
+        var animation = transform.GetChild(0);
+        animation.transform.parent = newShip.transform;
+
+        Destroy(gameObject);
+    }
+}
