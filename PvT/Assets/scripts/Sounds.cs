ef0cfd8a9d95511044f3c051d90030b3258fe2b5
Kai Arnold <cdn_kai@yahoo.com>
2014-11-23 19:03:49 -0800

    Adding some new sounds, plus a start to a possible Sound manager class.

---
+++ PvT/Assets/scripts/Sounds.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public sealed class Sounds
+{
+    public enum ActorEvent { SPAWN, DEATH, COLLISION } 
+    public enum GlobalEvent { OVERWHELM, PLAYER_DEATH, LEVELUP, EXPLOSION, ROAR }
+
+    readonly Dictionary<ActorType, AudioClip[][]> _lookup;
+    public void Add(ActorType actor, ActorEvent evt, AudioClip clip)
+    {
+        AudioClip[][] clips = null;
+        if (!_lookup.TryGetValue(actor, out clips))
+        {
+            clips = _lookup[actor] = new AudioClip[NUM_EVENTS][];
+        }
+    }
+    public AudioClip Get(ActorType actor, ActorEvent evt)
+    {
+
+        return null;
+    }
+    
+    readonly Dictionary<GlobalEvent, AudioClip[]> _globalEventLookup;
+    public void Add(GlobalEvent evt, AudioClip clip)
+    {
+    }
+    public AudioClip Get(GlobalEvent evt)
+    {
+        return null;
+    }
+
+    readonly int NUM_EVENTS;
+    public Sounds()
+    {
+        NUM_EVENTS = Enum.GetValues(typeof(ActorEvent)).Length - 1;
+    }
+}
