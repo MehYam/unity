88df7710f95d987466292c2340bce4609a434a99
Kai Arnold <cdn_kai@yahoo.com>
2015-02-16 16:01:09 -0800

    new dunjon and kart projects

---
+++ cycle/Assets/test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+    public GameObject target;
+
+	// Use this for initialization
+    Vector2 startPos;
+	void Start()
+    {
+	    startPos = target.transform.position;
+	}
+
+	public void ReplaceTarget()
+    {
+        target.transform.position = startPos;
+    }
+}
---
+++ dunjon/Assets/Map.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class Map : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public Sprite foo;
+
+	// Use this for initialization
+	void Start()
+    {
+	    var renderer = (SpriteRenderer) tilePrefab.renderer;
+
+        Debug.Log(renderer.sprite.rect);
+        Debug.Log(renderer.sprite.textureRect);
+
+        var obj = (GameObject)Instantiate(tilePrefab);
+        renderer = (SpriteRenderer) obj.renderer;
+        //renderer.sprite.textureRect = new Rect(50, 50, 150, 150);
+    }
+}
