276d50dc6d083bda2ee3111fa12dc7aa2a471fae
Kai Arnold <cdn_kai@yahoo.com>
2015-03-09 15:37:51 -0700

    Move most of the logic from HerolingActor => Heroling.  Capture logic not yet migrated.

--- PvT/Assets/scripts/Actors/Heroling.cs
+++ PvT/Assets/scripts/Actors/Heroling.cs
@@ -1,15 +1,133 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Heroling : MonoBehaviour {
+using PvT.DOM;
+using PvT.Util;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public sealed class Heroling : MonoBehaviour
+{
+    static public int ActiveHerolings { get; private set; }
+    static public void ReturnAll()
+    {
+        var instances = GameObject.FindObjectsOfType<Heroling>();
+        foreach (var instance in instances)
+        {
+            instance.Return();
+        }
+    }
+    void OnEnable()
+    {
+        ++ActiveHerolings;
+    }
+    void OnDisable()
+    {
+        --ActiveHerolings;
+    }
+
+    Timer? _roamBoredom;
+    void Start()
+    {
+        _roamBoredom = new Timer(Consts.HEROLING_ROAM_BOREDOM);
+
+        SetBehavior(ROAM);
+
+        // make it appear on top of mobs and friendlies
+        GetComponent<SpriteRenderer>().sortingLayerID = (int)Consts.SortingLayer.AMMO_TOP;
+
+        GetComponent<Actor>().target = PlayerTarget.Instance;
+
+        GlobalGameEvent.Instance.FireHerolingLaunched();
+    }
+
+    static ActorAttrs SPEED_BOOST = null;  // not set at static time because we need to load the ActorAttrs from the config
+    void SetBehavior(IActorBehavior b, bool speedBoost = false)
+    {
+        var actor = GetComponent<Actor>();
+        actor.behavior = b;
+
+        if (speedBoost)
+        {
+            if (SPEED_BOOST == null)
+            {
+                SPEED_BOOST = new ActorAttrs(2 * actor.actorType.attrs.maxSpeed, 0, 0);
+            }
+            actor.AddActorModifier(SPEED_BOOST);
+        }
+        else if (SPEED_BOOST != null)
+        {
+            actor.RemoveActorModifier(SPEED_BOOST);
+        }
+    }
+
+    Timer? _attachBoredom;
+    void AttachToObject(Transform other)
+    {
+        var otherActor = other.GetComponent<Actor>();
+        if (otherActor.isCapturable)
+        {
+            // attach
+            transform.parent = other;
+
+            // sidle up
+            var gimmeAKiss = transform.localPosition + Util.ScatterRandomly(0.25f);
+            gimmeAKiss.Scale(new Vector3(0.4f, 0.4f));
+            transform.localPosition = gimmeAKiss;
+
+            // disable physics
+            Util.DisablePhysics(gameObject);
+
+            SetBehavior(ATTACHED);
+            _roamBoredom = null;
+            _attachBoredom = new Timer(Consts.HEROLING_ATTACH_BOREDOM);
+
+            ++other.GetComponent<Actor>().attachedHerolings;
+            GlobalGameEvent.Instance.FireHerolingAttached(other.GetComponent<Actor>());
+        }
+    }
+
+    void Return()
+    {
+        gameObject.layer = (int)Consts.CollisionLayer.HEROLINGS_RETURNING;
+        if (transform.parent != null)  //KAI: dorked, because all ammo's parented initially
+        {
+            var parentActor = transform.parent.GetComponent<Actor>();
+            if (parentActor != null)
+            {
+                transform.parent = null;
+
+                --parentActor.attachedHerolings;
+                GlobalGameEvent.Instance.FireHerolingDetached(parentActor);
+            }
+        }
+        // re-enable physics
+        Util.EnablePhysics(gameObject);
+
+        // go back home
+        SetBehavior(RETURN, true);
+    }
+
+    //KAI: better done via coroutines?
+    void FixedUpdate()
+    {
+        if (_roamBoredom != null && _roamBoredom.Value.reached)
+        {
+            Return();
+            _roamBoredom = null;
+        }
+        else if (_attachBoredom != null && _attachBoredom.Value.reached)
+        {
+            Return();
+            _attachBoredom = null;
+        }
+    }
+
+    static readonly IActorBehavior ROAM = new CompositeBehavior(
+        ActorBehaviorFactory.Instance.faceMouse,
+        ActorBehaviorFactory.Instance.thrust
+    );
+    static readonly IActorBehavior RETURN = new CompositeBehavior(
+        ActorBehaviorFactory.Instance.faceForward,
+        ActorBehaviorFactory.Instance.homeToTarget
+    );
+    static readonly IActorBehavior ATTACHED = null;
 }
