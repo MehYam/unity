bbd51654c007ea3277aa04cc520e2d2f85e03605
Kai Arnold <cdn_kai@yahoo.com>
2015-03-10 17:42:56 -0700

    Add Tank class to get the tread animations synced.

---
+++ PvT/Assets/scripts/Actors/Tank.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class Tank : MonoBehaviour
+{
+    Animator left;
+    Animator right;
+    Rigidbody2D body;
+	void Start()
+    {
+        left = transform.FindChild("tanktreadParentLeft").GetComponentInChildren<Animator>();
+        right = transform.FindChild("tanktreadParentRight").GetComponentInChildren<Animator>();
+
+        left.applyRootMotion = false;
+        right.applyRootMotion = false;
+
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        left.speed = right.speed = body.velocity.sqrMagnitude * 2;
+    }
+}
