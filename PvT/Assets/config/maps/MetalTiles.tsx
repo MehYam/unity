ee5099849e0b290453d615e3e08ffa237f28ae1f
Kai Arnold <cdn.kai@gmail.com>
2016-04-19 19:10:30 -0700

    Various serialization changes in the 5.0 -> 5.3 upgrade.

--- PvT/Assets/config/maps/MetalTiles.tsx
+++ PvT/Assets/config/maps/MetalTiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="MetalTiles" tilewidth="128" tileheight="128">
- <image source="../../../../../assets/MetalTiles.png" width="512" height="1024"/>
+ <image source="../../images/MetalTiles.png" width="512" height="1024"/>
  <tile id="2">
   <objectgroup draworder="index">
    <object x="0" y="0" width="128" height="128"/>
