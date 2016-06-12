e9892bffd49d0d9138b5b296f94be7f3975817b4
Kai Arnold <cdn.kai@gmail.com>
2016-06-12 13:30:52 -0700

    face mouse on fire.  Breaks the nice tilting

--- test/Assets/scripts/FaceForward.cs
+++ test/Assets/scripts/FaceForward.cs
@@ -11,14 +11,31 @@ public sealed class FaceForward : MonoBehaviour
     {
         return DegreesRotationAboutY(point);
     }
+    static private Vector3 MouseWorldCoordinates
+    {
+        get
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = Camera.main.nearClipPlane;
+
+            return Camera.main.ScreenToWorldPoint(mousePos);
+        }
+    }
 	void FixedUpdate()
     {
+        var angles = gameObject.transform.eulerAngles;
         var rb = gameObject.GetComponent<Rigidbody>();
-        if (rb.velocity != Vector3.zero)
+
+        if (Input.GetButton("Fire1"))
         {
-            var angles = gameObject.transform.eulerAngles;
-            angles.y = GetLookAtAngle(rb.velocity);
-            gameObject.transform.eulerAngles = angles;
+            Debug.Log(Input.mousePosition + ", " + MouseWorldCoordinates);
+
+            angles.y = GetLookAtAngle(MouseWorldCoordinates - gameObject.transform.position);
         }
-	}
+        else if (rb.velocity != Vector3.zero)
+        {
+            angles.y = GetLookAtAngle(rb.velocity);
+        }
+        gameObject.transform.eulerAngles = angles;
+    }
 }
