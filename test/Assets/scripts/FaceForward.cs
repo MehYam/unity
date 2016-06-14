df36c4a0d243663bb112dc1ea82329493fb0ce3a
Kai Arnold <cdn.kai@gmail.com>
2016-06-13 21:31:57 -0700

    fix incorrect mapping of screen/mouse coordinates to XZ plane

--- test/Assets/scripts/FaceForward.cs
+++ test/Assets/scripts/FaceForward.cs
@@ -5,6 +5,11 @@ public sealed class FaceForward : MonoBehaviour
 {
     public float rotationalLimit = 100;
 
+    static private float DegreesRotationXZPlane(Vector2 point)
+    {
+        // This assumes our camera is looking down on the XZ plane
+        return Mathf.Atan2(point.x, point.y) * Mathf.Rad2Deg;
+    }
     static private float DegreesRotationAboutY(Vector3 point)
     {
         return Mathf.Atan2(point.x, point.z) * Mathf.Rad2Deg;
@@ -12,6 +17,15 @@ public sealed class FaceForward : MonoBehaviour
     static private float GetLookAtAngle(Vector3 point)
     {
         return DegreesRotationAboutY(point);
+    }
+    static private float GetLookAtAngleToMouse(Vector3 point)
+    {
+        // To do mouse stuff in world coordinates instead:  http://wiki.unity3d.com/index.php?title=LookAtMouse
+        // I find it's more intuitive and natural to transform objects to screen coordinates instead of trying
+        // to translate mouse coordinates into world space, since that's generally the intent of using mouse
+        // coordinates in a top-down 2D-style game.
+        var screenPoint = Camera.main.WorldToScreenPoint(point);
+        return DegreesRotationXZPlane(Input.mousePosition - screenPoint);
     }
     static private Vector3 MouseWorldCoordinates
     {
@@ -31,7 +45,7 @@ public sealed class FaceForward : MonoBehaviour
         float targetAngleY = angles.y;
         if (Input.GetButton("Fire1"))
         {
-            targetAngleY = GetLookAtAngle(MouseWorldCoordinates - gameObject.transform.position);
+            targetAngleY = GetLookAtAngleToMouse(gameObject.transform.position);
         }
         else if (rb.velocity != Vector3.zero && PlayerInput.UNDER_FORCE)
         {
