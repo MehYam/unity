ea755150c0f4f383cdeb12d83ce86cec223a03cd
Kai Arnold <cdn.kai@gmail.com>
2016-06-13 22:35:58 -0700

    add primitive weapon fire

---
+++ test/Assets/scripts/FireWeapon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class FireWeapon : MonoBehaviour
+{
+    public GameObject firepoint;
+    public GameObject ammo;
+    public float rate = 1;
+
+    float _lastFire = 0;
+	void FixedUpdate()
+    {
+	    if (Input.GetButton("Fire1") && (Time.fixedTime - _lastFire) > rate)
+        {
+            var shot = GameObject.Instantiate(ammo);
+            shot.transform.position = firepoint.transform.position;
+
+            var rb = shot.transform.GetComponent<Rigidbody>();
+            rb.velocity = gameObject.GetComponent<Rigidbody>().velocity;
+
+            Debug.Log(rb.velocity);
+
+            rb.angularVelocity = Vector3.up * 10;
+
+            _lastFire = Time.fixedTime;
+        }
+	}
+}
