031c25c9254a35f9d3f99086d65e36dd389eafbb
Kai Arnold <cdn.kai@gmail.com>
2016-06-21 18:32:48 -0700

    add level-off logic for height smoothing

---
+++ test/Assets/scripts/LevelOffAngle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class LevelOffAngle : MonoBehaviour
+{
+    float velocityX = 0;
+    float velocityZ = 0;
+	void FixedUpdate()
+    {
+        var angles = gameObject.transform.eulerAngles;
+
+        angles.x = Mathf.SmoothDampAngle(angles.x, 0, ref velocityX, 1);
+        angles.z = Mathf.SmoothDampAngle(angles.z, 0, ref velocityZ, 1);
+
+        gameObject.transform.eulerAngles = angles;
+    }
+}
---
+++ test/Assets/scripts/LevelOffHeight.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class LevelOffHeight : MonoBehaviour
+{
+    static private float EPSILON = 0.0001f;
+
+    public float height = 0;
+    public float speed = 1f;
+	void FixedUpdate()
+    {
+        var pos = gameObject.transform.position;
+        var delta = pos.y - height;
+
+        if (Mathf.Abs(delta) > EPSILON)
+        {
+            pos.y -= delta * speed * Time.fixedDeltaTime;
+        }
+        else
+        {
+            pos.y = height;
+        }
+        gameObject.transform.position = pos;
+    }
+}
