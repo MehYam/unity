8caf3ce178b5b474ccbd37e37bdc4f0b5abc9957
Kai Arnold <cdn.kai@gmail.com>
2016-06-23 20:06:27 -0700

    add first bit of AI:  face + thrust

---
+++ test/Assets/scripts/FaceTarget.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using PvT3D.Util;
+
+public sealed class FaceTarget : MonoBehaviour
+{
+    [Tooltip("The GameObject this object will LookAt()")]
+    public GameObject target;
+
+    //KAI: copy/pasta of FaceForward
+    [Tooltip("Maximum rotation speed in Rotations Per Second")]
+    public float maxRPS = 1;
+
+    void FixedUpdate()
+    {
+        // determine the target angle based on heading or mouse cursor
+        float targetAngleY = Util.DegreesRotationInY(target.transform.position - gameObject.transform.position);
+
+        // tween the rotation
+        var angles = gameObject.transform.eulerAngles;
+        var angleDelta = Mathf.DeltaAngle(angles.y, targetAngleY);
+        var maxRotationThisFrame = maxRPS * Time.fixedDeltaTime * 360;
+
+        angleDelta = Mathf.Clamp(angleDelta, -maxRotationThisFrame, maxRotationThisFrame);
+
+        angles.y = angles.y + angleDelta;
+        gameObject.transform.eulerAngles = angles;
+    }
+}
---
+++ test/Assets/scripts/Thrust.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class Thrust : MonoBehaviour
+{
+    public float acceleration = 1;
+    void FixedUpdate()
+    {
+        var direction = gameObject.transform.forward;
+        direction.y = 0;
+
+        gameObject.GetComponent<Rigidbody>().AddForce(direction * acceleration);
+    }
+}
