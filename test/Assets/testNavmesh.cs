cd9b26a453dce00000930d7b5df6b1f41bcabb7c
Kai Arnold <cdn.kai@gmail.com>
2016-07-25 14:17:09 -0700

    update ship3, test some NavMesh navigation (it works and is awesome)

---
+++ test/Assets/testNavmesh.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testNavmesh : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+
+        var agent = GetComponent<NavMeshAgent>();
+
+        //agent.destination = target.transform.position;
+        
+	}
+}
