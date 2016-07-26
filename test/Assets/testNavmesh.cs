8c2f2921c3fd7a23f2d8e325cbec2c9b7dbb7d4f
Kai Arnold <cdn.kai@gmail.com>
2016-07-26 13:27:07 -0700

    more models

--- test/Assets/testNavmesh.cs
+++ test/Assets/testNavmesh.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class testNavmesh : MonoBehaviour {
@@ -11,6 +11,10 @@ public class testNavmesh : MonoBehaviour {
         var agent = GetComponent<NavMeshAgent>();
 
         //agent.destination = target.transform.position;
+
+        var path = new NavMeshPath();
+        agent.CalculatePath(target.transform.position, path);
+
         
 	}
 }
