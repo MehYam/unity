643c0110fcde8eaaf00365f5d848ac27a4cbd004
Kai Arnold <cdn.kai@gmail.com>
2016-09-15 14:39:56 -0700

    render maps, add basic mouse controls to camera

--- life/Assets/Main.cs
+++ life/Assets/Main.cs
@@ -1,6 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using lifeEngine;
+using System.Collections.Generic;
+
 public class Main : MonoBehaviour
 {
+    public Sprite[] glyphs;
+    void Start()
+    {
+        Debug.Log("System.Environment.Version " + System.Environment.Version);
+
+        var layer = Operations.LoadLayerFile("c:\\source\\cs\\life\\simplerooms1.txt");
+        RenderLayer(layer);
+    }
+
+    void RenderLayer(Layer<Tile> layer)
+    {
+        var glyphLookup = new Dictionary<char, int>();
+        glyphLookup[' '] = 5;
+        glyphLookup['#'] = 6;
+        glyphLookup['.'] = 3;
+
+        Vector2 offset = new Vector2(layer.width / 2, layer.height / 2);
+        layer.ForEach((x, y, tile) =>
+        {
+            var sprite = glyphs[glyphLookup[tile.type]];
+
+            var gobj = new GameObject(string.Format("tile_{0:000}_{1:000}", x, y));
+            gobj.transform.parent = gameObject.transform;
+
+            var renderer = gobj.AddComponent<SpriteRenderer>();
+            renderer.sprite = sprite;
+
+            gobj.transform.position = new Vector2(x, y) - offset;
+        });
+    }
 }
---
+++ life/Assets/MouseControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class MouseControl : MonoBehaviour
+{
+    [Range(5, 150)]
+    public float zoomSensitivity = 50;
+    const float BASE_ZOOM_SENSITIVITY = 100;
+
+    Vector3 startPosition;
+    Vector3 origin;
+    Vector3 offset;
+    bool dragging = false;
+
+    void Start()
+    {
+        startPosition = Camera.main.transform.position;
+    }
+    void LateUpdate()
+    {
+        if (Input.GetMouseButton(2))
+        {
+            offset = (Camera.main.ScreenToWorldPoint(Input.mousePosition)) - Camera.main.transform.position;
+            if (dragging == false)
+            {
+                dragging = true;
+                origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+        }
+        else
+        {
+            dragging = false;
+        }
+        if (dragging == true)
+        {
+            Camera.main.transform.position = origin - offset;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Camera.main.transform.position = startPosition;
+        }
+        Camera.main.orthographicSize += Input.mouseScrollDelta.y * zoomSensitivity / BASE_ZOOM_SENSITIVITY;
+    }
+}
