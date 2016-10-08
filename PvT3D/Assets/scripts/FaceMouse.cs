72fa438bfdb17abad9142549f78daec86d82d5e0
Kai Arnold <cdn.kai@gmail.com>
2016-10-07 21:06:44 -0700

    make turret rotation smooth

--- PvT3D/Assets/scripts/FaceMouse.cs
+++ PvT3D/Assets/scripts/FaceMouse.cs
@@ -6,16 +6,16 @@ using PvT3D.Util;
 public sealed class FaceMouse : MonoBehaviour
 {
     //KAI: copy pasta with FaceForward - abstract how?  Some should swap between the FaceMouse and FaceForward behaviors, maybe
-    [Tooltip("Maximum rotation speed in Rotations Per Second")]
+    [Tooltip("Maximum rotation speed in Rotations Per Second.  -1 == instant rotation")]
     public float maxRPS = 1;
 
-    void FixedUpdate()
+    void Update()  //KAI: this is way smoother than doing this in FixedUpdate - review our FixedUpdate use.
     {
         var angles = gameObject.transform.eulerAngles;
         var targetAngleY = Util.DegreesRotationToMouseInY(gameObject.transform.position);
 
         var angleDelta = Mathf.DeltaAngle(angles.y, targetAngleY);
-        var maxRotationThisFrame = maxRPS * Time.fixedDeltaTime * 360;
+        var maxRotationThisFrame = maxRPS > 0 ? maxRPS * Time.fixedDeltaTime * 360 : 360;
 
         angleDelta = Mathf.Clamp(angleDelta, -maxRotationThisFrame, maxRotationThisFrame);
 
