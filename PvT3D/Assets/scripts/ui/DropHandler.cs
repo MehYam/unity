747213ea8dc2995c22c3f99a14191d045e49e036
Kai Arnold <cdn.kai@gmail.com>
2017-02-18 19:27:26 -0800

    prevent multiple items per slot in grid

--- PvT3D/Assets/scripts/ui/DropHandler.cs
+++ PvT3D/Assets/scripts/ui/DropHandler.cs
@@ -9,7 +9,11 @@ public sealed class DropHandler : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("OnDrop " + name);
-        eventData.pointerDrag.transform.SetParent(transform);
-        eventData.pointerDrag.transform.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+
+        if (GetComponentInChildren<DragHandler>() == null)
+        {
+            eventData.pointerDrag.transform.SetParent(transform);
+            eventData.pointerDrag.transform.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        }
     }
 }
