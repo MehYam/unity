f42ab840d1efa87e6ce4c312f2eb4166fce96e4f
Kai Arnold <cdn.kai@gmail.com>
2017-05-02 16:38:00 -0700

    create a proper AmmoBeam, improve the animation, parameterize it from inspector

--- PvT3D/Assets/scripts/AmmoBeam.cs
+++ PvT3D/Assets/scripts/AmmoBeam.cs
@@ -1,9 +1,25 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AmmoBeam : MonoBehaviour
 {
-    [SerializeField] AnimationCurve lengthCurve;
-    [SerializeField] AnimationCurve widthCurve;
+    [SerializeField] float duration = 1;
+    [SerializeField] float distance = 50;
+    [SerializeField] float width = 0.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // We're scaling the object up as an explosion beam effect, in order to grow the front and not the back,
+        // we must position the child cube so that it's back edge is lined up to our origin.
+        var visual = transform.GetChild(0);
+        visual.transform.localPosition = new Vector3(0, 0, visual.transform.localScale.z / 2);
+
+        var scale = transform.localScale;
+
+        LeanTween.scaleZ(gameObject, distance / visual.localScale.z, duration / 2).setEaseOutExpo();
+        LeanTween.scaleX(gameObject, width / visual.localScale.x, duration / 4);
+
+        LeanTween.alpha(visual.gameObject, 0, duration);
+    }
 }
